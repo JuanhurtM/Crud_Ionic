c1e6bdb457546817bdcb317b18e1ca8517baee99
Juan Hurtado <84244766+JuanhurtM@users.noreply.github.com>
2023-10-07 16:10:40 -0500

    Read and Create

---
+++ src/app/form/form.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.page.html',
+  styleUrls: ['./form.page.scss'],
+})
+export class FormPage implements OnInit {
+
+  datos:any={
+    nombre: '',
+    correo: '',
+    universidad: ''
+  }
+
+  constructor(private http: HttpClient, private Router: Router) { }
+
+  ngOnInit() {
+  }
+
+  save(){
+    this.http.get("http://localhost/IonicApi/src/controllers/guardar.php?nombre="+this.datos.nombre+"&correo="+this.datos.correo+"&universidad="+this.datos.universidad).subscribe(snap =>{
+        console.log(snap);
+        this.Router.navigate(['/home']);
+      });
+  }
+
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,13 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  listado!:any;
+
+  constructor(private http: HttpClient) {
+      this.http.get("http://localhost/IonicApi/src/controllers/consultados.php").subscribe(snap =>{
+        console.log(snap);
+        this.listado=snap;
+      });
+  }
 
 }
